a13043208c33d819fd58d177080fc51dc1638caa
Fabrício Herpich <fabricio.herpich@gmail.com>
2021-09-16 10:26:34 -0300

    Projeto contador

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# contador
+# Primeiro projeto - Contador de alunos
 
 Este é o primeiro projeto em Flutter desenvolvido durante a disciplina de Programação para Dispositivos Móveis (PDM38106) no Instituto Federal de Santa Catarina - Campus Caçador.
 
 O objetivo deste projeto era demonstrar aos alunos como criar um contador de pessoas, implementar widgets, as diferenças entre stateless e stateful, entre outras coisas.
 
-Para mais informações, acesse:
-[Documentação](https://flutter.dev/docs).
+Dúvidas sobre o projeto: fabricio.herpich@gmail.com
+
+Para mais informações sobre Flutter, acesse:
+[Documentação](https://flutter.dev/docs) e 
 [Exemplos de projetos](https://flutter.dev/docs/cookbook).
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _numAlunos = 0; //_ para indicar que é acessível apenas aqui dentro
+
+  void _changeAlunos(int delta) {
+    setState(() {
+      //  manda atualizar a tela (só a parte modificada do código)
+      _numAlunos += delta; //executar o incremento
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/sala.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        //agora adicione <como filho> a outra parte do código já desenvolvida
+        Column(
+          // inclua o widget coluna
+          mainAxisAlignment:
+              MainAxisAlignment.center, //centralizando os widgets
+          children: <Widget>[
+            //adicione um children
+            Text(
+              // e inclua um widget de text
+              "Pessoas: $_numAlunos", // adicione um texto
+              style: TextStyle(
+                  color: Colors.white, // cor do texto
+                  fontWeight: FontWeight.bold), //configure o estilo do texto
+            ),
+            Row(
+                //Crie uma linha dentro da coluna
+                mainAxisAlignment:
+                    MainAxisAlignment.center, //centralizando os widgets
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(
+                        10.0), //adicionando espaço igual nas laterais
+                    child: FlatButton(
+                      //adicione um FlatButton
+                      child: Text(
+                        //adicione o texto +1
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {
+                        _changeAlunos(1);
+                        //debugPrint("+1");
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(
+                        10.0), //adicionando espaço igual nas laterais
+                    child: FlatButton(
+                      //adicione um FlatButton
+                      child: Text(
+                        //adicione o texto +1
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {
+                        _changeAlunos(-1);
+                        //debugPrint("-1");
+                      },
+                    ),
+                  ),
+                ]),
+            Text(
+              // inclua um outro widget de texto
+              "Pode Entrar!", // adicione um texto
+              style: TextStyle(
+                  //configure o estilo do texto
+                  color: Colors.white, // cor do texto
+                  fontStyle: FontStyle
+                      .italic, //configure o estilo do texto e o tamanho
+                  fontSize: 30.0), //configure o tamanho
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
